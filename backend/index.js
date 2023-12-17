87e9bdc9113ec264005ac6d853ceebdd1ba04f70
ayush <ayushbhatt@204gmail.com>
2023-12-17 20:28:11 +0530

    first

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;  
+  // Get or create user on Chat Engine!
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "0c34fa2b-16f1-4dfc-b448-5302fe578022" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+
+app.listen(3001);
